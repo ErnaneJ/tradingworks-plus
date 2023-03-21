fbd07864626a7ad90156083b8fe891356bf21e8c
Ernane Ferreira <ernane.junior25@gmail.com>
2023-03-21 10:33:21 -0300

    fix: clicking on the links opens two new tabs

--- assets/scripts/initialize.js
+++ assets/scripts/initialize.js
@@ -1,17 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-  handleLinks();
   updateWorkedHours();
   handleButtonConfig();
 });
-
-function handleLinks(){
-  const links = document.querySelectorAll('a');
-  links.forEach(link => {
-    link.addEventListener('click', () => {
-      chrome.tabs.create({'url': link.href }, (tab) => { });
-    });
-  });
-}
 
 function handleButtonConfig(){
   const buttonConfig = document.getElementById('button-config');
