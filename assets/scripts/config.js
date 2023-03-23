d51ee131128afd928dab43cb2395301068d62334
Thiago <thiagodavidpd@gmail.com>
2023-03-23 14:58:51 -0300

    change type of input time to text and create  function to  format it  properly

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -15,6 +15,27 @@ function loadFormByData(){
   }
 }
 
+let hoursInputs = document.querySelectorAll("#work-time, #break-time")
+
+hoursInputs.forEach((input) => {
+  input.addEventListener('blur', (event) => {
+    const value = event.target.value.padStart(4, '0');
+    const hours = value.slice(0, 2);
+    const minutes = value.slice(2);
+
+
+    if ((hours !== '00' && minutes > 59) || (hours == 24 && minutes > 00 || hours > 24)) {
+      event.target.value = '00:00';
+      alert('Horas inválidas!');
+      return;
+    }
+
+    const formattedValue = value.includes(':') ? value : `${hours}:${minutes}`;
+    event.target.value = formattedValue;
+  });
+});
+
+
 function handleSubmit(){
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
