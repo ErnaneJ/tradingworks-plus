83fac1941f9dfde3cb4fac93902ed9f0cff50985
Thiago <thiagodavidpd@gmail.com>
2023-03-23 10:58:47 -0300

    remove unused function on this branch

--- assets/scripts/initialize.js
+++ assets/scripts/initialize.js
@@ -9,13 +9,6 @@ function handleButtonConfig(){
   buttonConfig.addEventListener('click', openConfig);
 }
 
-function handleButtonRefresh(){
-  const buttonRefresh = document.getElementById('button-refresh');
-  buttonRefresh.addEventListener('click', () => {
-    console.log('working')
-  });
-}
-
 function sendSettingsToBackgroundScript(){
   const settings = localStorage.getItem('tradingworks-plus-data');
   chrome.runtime.sendMessage({tradingworksPlusExtension: true, settings});
