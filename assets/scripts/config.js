f1f6f9d77e94ac448e9252ca4c223cef476aa28b
Ernane Ferreira <ernane.junior25@gmail.com>
2023-03-24 10:50:27 -0300

    ref: refactoring mask of configuration inputs

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -19,30 +19,49 @@ function loadFormByData(){
 function handleTimeInputs(){
   let hoursInputs = document.querySelectorAll("#work-time, #break-time")
 
-  hoursInputs.forEach((input) => {
-  input.addEventListener('blur', (event) => {
-    const value = event.target.value.padStart(4, '0');
-    const hours = value.slice(0, 2);
-    const minutes = value.slice(2);
+  const format = event => {
+    let value = event.target.value
+  
+    value = value.replace(/[a-zA-Z\:]/g, '');
+    console.log(value)
+    value = value.padStart(4, '0');
 
-
-    if ((hours !== '00' && minutes > 59) || (hours == 24 && minutes > 00 || hours > 24)) {
-      event.target.value = '00:00';
-      alert('Horas inválidas!');
-      return;
+    if (event.key === 'Backspace') {
+      event.preventDefault();
+      value = value.substring(0, value.length);
+      value = value.padStart(4, '0');
+    } else if("1234567890".includes(event.key)){
+      value = value.substring(1);
+      value = value.padEnd(4, '0');
+    } else{
+      event.preventDefault();
     }
 
-    const formattedValue = value.includes(':') ? value : `${hours}:${minutes}`;
-    event.target.value = formattedValue;
-  });
-});
-}
+    event.target.value = value.replace(/(\d{2})(\d{2})/, '$1:$2');
 
+    const hours = parseInt(event.target.value.split(':')[0]);
+    const minutes = parseInt(event.target.value.split(':')[1]);
+
+    if(hours > 23 || minutes > 59) {
+      document.querySelector('form').classList.add('invalid');
+      event.target.style.outline = '2px solid #db4444';
+    }else{
+      event.target.style.outline = 'initial';
+    }
+  }
+
+  hoursInputs.forEach((input) => {
+    input.addEventListener('keyup', format);
+  });
+}
 
 function handleSubmit(){
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    if(event.target.classList.contains('invalid')) return alert('Erro ao salvar. Verifique as informações e tente novamente. 😢');
+
     const data = new FormData(event.target);
     const formatedData = Object.fromEntries(data.entries());
 
